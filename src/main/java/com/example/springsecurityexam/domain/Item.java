a6e6da7a7ff29da6150f70bc4c1cc6d8d4704d1c
Sung min <okt0517@gmail.com>
2023-01-17 11:08:09 +0900

    add to items view but only html

--- src/main/java/com/example/springsecurityexam/domain/Item.java
+++ src/main/java/com/example/springsecurityexam/domain/Item.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @NoArgsConstructor
 @Entity
@@ -27,6 +29,12 @@ public class Item {
 
     @Column(nullable = false)
     private int producer;
+
+    @Column(nullable = false, insertable = false, updatable = false)
+    private Date createdDate;
+
+    @Column(nullable = false, insertable = false, updatable = false)
+    private Date updatedDate;
 
     @Builder
     public Item(String itemName, int price, int quantity){
---
+++ src/main/java/com/example/springsecurityexam/dto/ItemUpdateDto.java
@@ -0,0 +1,4 @@
+package com.example.springsecurityexam.dto;
+
+public class ItemUpdateDto {
+}
